143d5b65c227cf111161c92a80a2bacd2c779848
rooooooooob <raftias@gmail.com>
2016-06-05 19:24:53 -0700

    fixed it! now it renders a sphere in flat blue! stupid parentheses...

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import Data.Maybe
 import Linear.V3
 import Linear.Vector
 import Linear.Metric
+import Debug.Trace
 
 data Geometry = Sphere {
     center :: V3 Float,
@@ -16,16 +17,16 @@ data Ray = Ray {
     dir :: V3 Float
 }
 
-main = savePngImage "output.png" $ ImageRGBF (generateImage (\x y -> screenPixel x y 640 480) 640 480)
+main = savePngImage "output.png" $ ImageRGBF (generateImage (\x y -> screenPixel x y 320 320) 320 320)
 
 screenPixel :: Int -> Int -> Int -> Int -> PixelRGBF
 screenPixel x y w h = traceRay (screenRay (fromIntegral x) (fromIntegral y) (fromIntegral w) (fromIntegral h)) sampleScene
 
-sampleCamera = Ray (V3 0 24 26) $ normalize (V3 0 (-0.9) (-1))
+sampleCamera = Ray (V3 0 0 1) (V3 0 0 (-1))--Ray (V3 0 24 26) $ normalize (V3 0 (-0.9) (-1))
 
 screenRay :: Float -> Float -> Float -> Float -> Ray
 screenRay x y w h = 
-    let xfov = pi / 2 :: Float -- 90 degree horizontal fov
+    let xfov = (pi / 4) :: Float -- 90 degree horizontal fov
         yfov = atan ((tan xfov) * (h / w)) :: Float
         xFromMid = ((x / w)) - 0.5 :: Float
         yFromMid = ((y / h)) - 0.5 :: Float
@@ -34,9 +35,9 @@ screenRay x y w h =
         yAxis = normalize (cross (dir sampleCamera) xAxis) :: V3 Float
         xComponent = ((tan xfov) * xFromMid) *^ xAxis :: V3 Float
         yComponent = ((tan yfov) * yFromMid) *^ yAxis :: V3 Float
-    in Ray (pos sampleCamera) ((xComponent + yComponent) - ((pos sampleCamera) + (dir sampleCamera)))
+    in Ray (pos sampleCamera) ((dir sampleCamera) + xComponent + yComponent)
 
-sampleScene = [Sphere (V3 0 0 0) 0.013]
+sampleScene = [Sphere (V3 0 0 0) 0.4]
 
 traceRay :: Ray -> [Geometry] -> PixelRGBF
 traceRay ray geometry = 
@@ -46,14 +47,14 @@ traceRay ray geometry =
 -- returns Just t if the distance from the ray origin to the object is t, otherwise Nothing
 intersects :: Ray -> Geometry -> Maybe Float
 intersects (Ray p d) (Sphere c r) =
-    let descrim = (dot d (p-c))*(dot d (p-c)) - (dot d d)*(dot (p-c) (p-c)) - r*r :: Float
-    in if descrim < 0.0 then Just sphereIntersection else Nothing
+    let descrim = (dot d (p-c))*(dot d (p-c)) - (dot d d)*((dot (p-c) (p-c)) - r*r) :: Float
+    in if descrim < 0.0 then Nothing else Just sphereIntersection
     where
         sphereIntersection :: Float
         sphereIntersection =
             let minusB = dot (-1.0 *^ d) (p - c) :: Float
-                denom = dot d d :: Float
-                descrim = (dot d (p-c))*(dot d (p-c)) - (dot d d)*(dot (p-c) (p-c)) - r*r :: Float
+                denom = (dot d d) :: Float
+                descrim = (dot d (p-c))*(dot d (p-c)) - (dot d d)*((dot (p-c) (p-c)) - r*r) :: Float
                 descrimSqrt = sqrt(descrim) :: Float
                 t1 = (minusB - descrimSqrt) / denom :: Float
                 t2 = (minusB + descrimSqrt) / denom :: Float
