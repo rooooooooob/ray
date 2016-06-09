26d2cffa66c8b5a6c04cbc11e7edac769dc3cc3c
rooooooooob <raftias@gmail.com>
2016-06-09 16:22:07 -0700

    added in triangle-ray collisions, although for some reason they only appear in reflections/refractions...

--- src/Main.hs
+++ src/Main.hs
@@ -11,6 +11,8 @@ import System.Random
 data Geometry = Sphere {
     center :: V3 Float,
     rad :: Float
+} | Triangle {
+    a, b, c :: V3 Float
 }
 
 data Ray = Ray {
@@ -45,23 +47,26 @@ instance Eq Object where
     x == y = (center.geo) x == (center.geo) y
 
 black = PixelRGBF 0 0 0
-
-main = savePngImage "output.png" $ ImageRGBF (generateImage (\x y -> antialiasedScreenPixel (zip [0] [0]) (fromIntegral x) (fromIntegral y) 800 600) 800 600)
+--[(xo, yo) | xo <- [-0.4, 0, 0.4], yo <- [-0.4, 0, 0.4]]
+main = savePngImage "output.png" $ ImageRGBF (generateImage (\x y -> screenPixel (fromIntegral x) (fromIntegral y) 1280 720) 1280 720)
 
 antialiasedScreenPixel :: [(Float, Float)] -> Float -> Float -> Int -> Int -> PixelRGBF
 antialiasedScreenPixel offsets x y w h = mixcs (foldl1 addcc [(screenPixel (x + ox) (y + oy) w h) | (ox, oy) <- offsets]) (1.0 / fromIntegral (length offsets))
 
 screenPixel :: Float -> Float -> Int -> Int -> PixelRGBF
-screenPixel x y w h = traceRay (screenRay x y (fromIntegral w) (fromIntegral h)) sampleScene sampleLights 2
+screenPixel x y w h = traceRay (screenRay x y (fromIntegral w) (fromIntegral h)) sampleScene sampleLights 1
 
-sampleCamera = Ray (V3 0 (6) (7)) $ normalize (V3 0 (-0.6) (-0.7))
+sampleCamera = (V3 0 (6) (7)) $ normalize (V3 0 (-0.6) (-0.7))
 sampleScene = addRandomSpheres 32 (mkStdGen 42) (Material (PixelRGBF 0.5 0.55 0.7) 0 2 0.05 0.02 1337) $
     addRandomSpheres 32 (mkStdGen 23) (Material (PixelRGBF 0.5 0.75 1) 1 1 0.05 0.7 320)
     [ Object (Sphere (V3 0 0 0) 1.3) (Material (PixelRGBF 0.3 0.3 0.3) 0.3 1.5 0.005 0.2 2560)
     , Object (Sphere (V3 0.5 0.5 2) 0.8) (Material (PixelRGBF 1 0.2 0.2) 0.3 1 1 0 0)
     , Object (Sphere (V3 0.85 1.5 1) 0.3) (Material (PixelRGBF 0.2 1 0.2) 0.1 1 1 0 0)
     , Object (Sphere (V3 0 2 0) 0.4) (Material (PixelRGBF 0.2 0.2 1) 0.8 1 0.4 0 500)
-    , Object (Sphere (V3 (-2) (-3) (-3)) 1) (Material (PixelRGBF 0.7 0.5 1) 0.5 1 0.1 3 600) ]
+    , Object (Sphere (V3 (-2) (-3) (-3)) 1) (Material (PixelRGBF 0.7 0.5 1) 0.5 1 0.1 3 600)
+    , Object (Triangle (V3 (-2) 0 2) (V3 2 0 2) (V3 0 2 0)) (Material (PixelRGBF 1 0.8 0.2) 1 2 1 0 0) ]
+    --, Object (Triangle (V3 (-200) (-20) 200) (V3 (-200) (-20) (-200)) (V3 (-200) (-20) 200)) (Material (PixelRGBF 0.3 1 1) 1 1 1 0 0)
+    --, Object (Triangle (V3 200 (-20) 200) (V3 (-200) (-20) 200) (V3 200 (-20) (-200))) (Material (PixelRGBF 1 0.3 1) 1 1 1 0 0) ]
 sampleLights = [ (PointLight (V3 (-5) 4 5.5) 6 (PixelRGBF 1 0.8 0.3))
     , (PointLight (V3 0 3.5 1.5) 3 (PixelRGBF 0.36 1.08 1.42))
     , (DirectionalLight (V3 0.2 (-1) 0) (PixelRGBF 0.5 0.5 0.5)) ]
@@ -131,32 +136,42 @@ refractTrace ray contactPos enterNorm obj objects lights = traceRay exitRay obje
 
 lightContrib :: V3 Float -> V3 Float -> V3 Float -> Object -> [Object] -> Light -> PixelRGBF
 lightContrib iray spos snorm obj objects light = 
-    let lightSpecificStuff :: Light -> (V3 Float, Float, Float)
-        lightSpecificStuff (PointLight lpos lrad lcol) =
-            let distToLight = distance lpos spos
-                distContrib = 1 - (min 1 $ distToLight / lrad)
-            in (normalize $ lpos - spos, distContrib, distToLight)
-        lightSpecificStuff (DirectionalLight ldir lcol) = (normalize $ (-1) *^ ldir, 1, read "Infinity")
-        (toLight, intensity, dist) = lightSpecificStuff light
-        hit = intersectsObjects (Ray (spos + 0.01 *^ snorm) toLight) objects
-        (t, _, _) = fromJust hit
+    let (toLight, intensity, dist) = case light of
+            (PointLight lpos lrad lcol) ->
+                let distToLight = distance lpos spos
+                    distContrib = 1 - (min 1 $ distToLight / lrad)
+                in (normalize $ lpos - spos, distContrib, distToLight)
+            (DirectionalLight ldir lcol) -> (normalize $ (-1) *^ ldir, 1, read "Infinity")
         diffuse lcol = mixcs (mixcc (col.mat $ obj) lcol) $ (mdif.mat $ obj)*(max 0 $ dot snorm toLight)
         specular lcol = mixcs (mixcc (col.mat $ obj) lcol) $ (mspec.mat $ obj)*(max 0 $ dot snorm $ normalize $ iray + toLight) ** (mspec.mat $ obj)
-    in if isNothing hit
+    in if isNothing $ intersectsObjects (Ray (spos + 0.01 *^ snorm) toLight) objects
        then mixcs (addcc (diffuse $ lcol light) (specular $ lcol light)) intensity
        else black
 
 -- returns Just t if the distance from the ray origin to the object is t, otherwise Nothing
 intersects :: Ray -> Object -> Maybe (Float, V3 Float, Object)
-intersects (Ray p d) obj = case geo obj of
+intersects (Ray o d) obj = case geo obj of
     (Sphere c r) ->
-        let minusB = -dot d (p - c) :: Float
+        let minusB = -dot d (o - c) :: Float
             denom = dot d d :: Float
-            descrim = (dot d (p-c))*(dot d (p-c)) - (dot d d)*((dot (p-c) (p-c)) - r*r) :: Float
+            descrim = (dot d (o-c))*(dot d (o-c)) - (dot d d)*((dot (o-c) (o-c)) - r*r) :: Float
             descrimSqrt = sqrt descrim :: Float
             t1 = (minusB - descrimSqrt) / denom :: Float
             t2 = (minusB + descrimSqrt) / denom :: Float
             t = if min t1 t2 >= 0 then min t1 t2 else max t1 t2 :: Float
         in if descrim < 0 || t < 0
            then Nothing
-           else Just (t, normalize $ (p + d^*t) - c, obj)
+           else Just (t, normalize $ (o + d^*t) - c, obj)
+    (Triangle a b c) ->
+        let ab = (b - a)
+            ac = (c - a)
+            n = cross ab ac
+            ndrd = dot n d :: Float
+            t = ((dot n o) + (dot a n))/ndrd :: Float
+            p = o + t*^d
+            atest = cross (b - a) (p - a) 
+            btest = cross (c - b) (p - b) 
+            ctest = cross (a - c) (p - c)
+        in if abs ndrd < 0.01 || t < 0 || (dot n atest) < 0 || (dot n btest) < 0 || (dot n ctest) < 0--) && (isNothing $ intersects (Ray p d) (Object (Sphere a 0.3) (Material (PixelRGBF 1 0 0) 0.3 1.5 0.005 0.2 2560))) && (isNothing $ intersects (Ray p d) (Object (Sphere b 0.3) (Material (PixelRGBF 0.3 0.3 0.3) 0.3 1.5 0.005 0.2 2560))) && (isNothing $ intersects (Ray p d) (Object (Sphere c 0.3) (Material (PixelRGBF 0.3 0.3 0.3) 0.3 1.5 0.005 0.2 2560)))
+           then Nothing
+           else Just (t, normalize n, obj)
