acae4d5e77a5be12d07eb4c40ccd8ae8f4cbacd7
rooooooooob <raftias@gmail.com>
2016-06-08 18:44:05 -0700

    I am going insane... but it works now? Must be a compiler bug on the Windows port, since it works fine on Linux on identical code, and if I pass in norms instead of computin functions it works on Windows, despite the vectors being identical (I tested for this)... oh well

--- src/Main.hs
+++ src/Main.hs
@@ -44,16 +44,16 @@ data Light = PointLight {
 instance Eq Object where
     x == y = (center.geo) x == (center.geo) y
 
-main = savePngImage "output.png" $ ImageRGBF (generateImage (\x y -> antialiasedScreenPixel [(ox, oy) | ox <- [-0.4, 0, 0.4], oy <- [-0.4, 0, 0.4]] (fromIntegral x) (fromIntegral y) 1280 720) 1280 720)
+main = savePngImage "output.png" $ ImageRGBF (generateImage (\x y -> antialiasedScreenPixel (zip [0] [0]) (fromIntegral x) (fromIntegral y) 800 600) 800 600)
 
 antialiasedScreenPixel :: [(Float, Float)] -> Float -> Float -> Int -> Int -> PixelRGBF
 antialiasedScreenPixel offsets x y w h = mixcs (foldl1 addcc [(screenPixel (x + ox) (y + oy) w h) | (ox, oy) <- offsets]) (1.0 / fromIntegral (length offsets))
 
 screenPixel :: Float -> Float -> Int -> Int -> PixelRGBF
-screenPixel x y w h = traceRay (screenRay x y (fromIntegral w) (fromIntegral h)) sampleScene sampleLights 3
+screenPixel x y w h = traceRay (screenRay x y (fromIntegral w) (fromIntegral h)) sampleScene sampleLights 2
 
 sampleCamera = Ray (V3 0 (6) (7)) $ normalize (V3 0 (-0.6) (-0.7))
-sampleScene = addRandomSpheres 32 (mkStdGen 42) (Material (PixelRGBF 0.5 0.55 0.7) 0.2 2 0.05 0.02 1337) $
+sampleScene = addRandomSpheres 32 (mkStdGen 42) (Material (PixelRGBF 0.5 0.55 0.7) 0 2 0.05 0.02 1337) $
     addRandomSpheres 32 (mkStdGen 23) (Material (PixelRGBF 0.5 0.75 1) 1 1 0.05 0.7 320)
     [ Object (Sphere (V3 0 0 0) 1.3) (Material (PixelRGBF 0.3 0.3 0.3) 0.3 1.5 0.005 0.2 2560)
     , Object (Sphere (V3 0.5 0.5 2) 0.8) (Material (PixelRGBF 1 0.2 0.2) 0.3 1 1 0 0)
@@ -98,21 +98,21 @@ addcc (PixelRGBF ra ga ba) (PixelRGBF rb gb bb) = PixelRGBF (ra+rb) (ga+gb) (ba+
 intersectsObjects :: Ray -> [Object] -> Maybe (Float, V3 Float, Object)
 intersectsObjects ray objects =
     let intersections = zip (map ((intersects ray) . geo) objects) objects :: [(Maybe (Float, V3 Float), Object)]
-        collided = map (\(res, o) -> (fst $ fromJust res, snd $ fromJust res, o)) $ filter (\(res, obj) -> isJust res) intersections :: [(Float, V3 Float, Object)]
+        collided = map (\(hit, o) -> (fst$fromJust hit, snd$fromJust hit, o)) $ filter (\(hit, obj) -> isJust hit) intersections :: [(Float, V3 Float, Object)]
     in if null collided
        then Nothing
-       else Just $ minimumBy (\(t1, _, _) (t2, _, _) -> compare t1 t2) collided
+       else Just $ minimumBy (\(t1, n1, o1) (t2, n2, o2) -> compare t1 t2) collided
 
 traceRay :: Ray -> [Object] -> [Light] -> Int -> PixelRGBF
 traceRay ray objects lights depth = case intersectsObjects ray objects of
-    Just (t, norm, obj) -> addcc (mixcs (col.mat $ obj) 0.1) (onCollide ray ((pos ray) + ((dir ray) ^* t)) norm obj objects lights depth)
+    Just (t, n, o) -> addcc (mixcs (col.mat $ o) 0.1) (onCollide ray ((pos ray) + ((dir ray) ^* t)) n o objects lights depth)
     Nothing -> PixelRGBF 0 0 0
 
 blackOr :: Bool -> PixelRGBF -> PixelRGBF
 blackOr cond def = if cond then def else PixelRGBF 0 0 0
 
 onCollide :: Ray -> V3 Float -> V3 Float -> Object -> [Object] -> [Light] -> Int -> PixelRGBF
-onCollide ray contactPos snorm obj objects lights depth =
+onCollide ray contactPos snorm obj objects lights depth = 
     let base = foldl combineMaybeCols Nothing lights
         reflectRay = Ray (contactPos + 0.01 *^ snorm) $ normalize $ (dir ray) - (project snorm (dir ray)) ^* 2
         reflectCol = blackOr (depth > 0 && (reflect.mat $ obj) > 0) $ mixcs (traceRay reflectRay objects lights (depth - 1)) (reflect.mat $ obj)
@@ -129,11 +129,11 @@ onCollide ray contactPos snorm obj objects lights depth =
 refractTrace :: Ray -> V3 Float -> V3 Float -> Object -> [Object] -> [Light] -> PixelRGBF
 refractTrace ray contactPos enterNorm obj objects lights =
     let enterRay = Ray (contactPos - 0.01 *^ enterNorm) $ refractRay 1 (refract.mat $ obj) enterNorm ((-1) *^ (dir ray)) :: Ray
-        out = intersects enterRay $ geo obj
-        (exitT, exitNorm) = fromJust $ out
-        exitPos = (pos enterRay) + ((dir enterRay) ^* exitT) :: V3 Float
+        (exitT, returnedNorm) = fromJust $ intersects enterRay $ geo obj
+        exitNorm = -1.0 *^ returnedNorm--(surfaceNorm (geo obj) exitPos) :: V3 Float
+        exitPos = (pos enterRay) + (dir enterRay) ^* exitT
         exitRay = Ray (exitPos - 0.01 *^exitNorm) $ refractRay (refract.mat $ obj) 1 exitNorm ((-1) *^ (dir enterRay)) :: Ray
-    in if isJust out then traceRay exitRay objects lights 0 else PixelRGBF 1 0 1
+    in traceRay exitRay objects lights 0
     where refractRay :: Float -> Float -> V3 Float -> V3 Float -> V3 Float
           refractRay n1 n2 snorm iray = ((n1 / n2)*(dot snorm iray) - (sqrt (1 - ((n1 / n2)*(n1 / n2))*(1 - (dot snorm iray)*(dot snorm iray)))))*^snorm - (n1 / n2)*^iray
 
@@ -169,7 +169,5 @@ intersects (Ray p d) (Sphere c r) =
                 descrimSqrt = sqrt descrim :: Float
                 t1 = (minusB - descrimSqrt) / denom :: Float
                 t2 = (minusB + descrimSqrt) / denom :: Float
-                (t, norm) = if t1 < 0
-                               then (t2, normalize $ c - (p + t2*^d))
-                               else (t1, normalize $ (p + t1*^d) - c)
-            in if t >= 0 then Just (t, norm) else Nothing
+                t = if min t1 t2 >= 0 then min t1 t2 else max t1 t2 :: Float
+            in if t >= 0 then Just (t, normalize $ (p + d^*t) - c) else Nothing
